f3b8fd3299237c27e1e8b4b140aaba47a2f03dde
Katherine Lin <85188042+katherinelc321@users.noreply.github.com>
2022-03-07 11:52:56 -0500

    set up labels reconciler (#4)
    * set up labels reconciler
    
    * add loop to propagate labels
    
    * refactor reconciler
    
    * fix pkg
    
    * fix looping
    
    * delete watch
    
    * add
    
    * refactor
    
    * refactor
    
    * scope down ns
    
    * fix logs
    
    * add label to nodes
    
    * add delete labels
    
    * refactor into func
    
    * add unit tests
    
    * fix boilerplate (#1)
    
    Co-authored-by: Katherine Lin <85188042+katherinelc321@users.noreply.github.com>
    
    * add note above failed test
    
    * Fix deploy target (#2)
    
    * fix boilerplate
    
    * Fix deploy target
    
    Co-authored-by: Manuel Dewald <github@manueldewald.de>

---
+++ controllers/machine_controller_test.go
@@ -0,0 +1,391 @@
+package controllers
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/golang/mock/gomock"
+	machinev1 "github.com/openshift/api/machine/v1beta1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/tools/record"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+type mocks struct {
+	fakeKubeClient client.Client
+	mockCtrl       *gomock.Controller
+}
+
+var _ reconcile.Reconciler = &ReconcileMachineSet{}
+
+func TestShouldExcludeMachine(t *testing.T) {
+	controller := true
+	testCases := []struct {
+		machineSet machinev1.MachineSet
+		machine    machinev1.Machine
+		expected   bool
+	}{
+		{
+			machineSet: machinev1.MachineSet{},
+			machine: machinev1.Machine{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "withNoMatchingOwnerRef",
+					Namespace: "test",
+					OwnerReferences: []metav1.OwnerReference{
+						{
+							Name:       "Owner",
+							Kind:       "MachineSet",
+							Controller: &controller,
+						},
+					},
+				},
+			},
+			expected: true,
+		},
+		{
+			machineSet: machinev1.MachineSet{
+				Spec: machinev1.MachineSetSpec{
+					Selector: metav1.LabelSelector{
+						MatchLabels: map[string]string{
+							"foo": "bar",
+						},
+					},
+				},
+			},
+			machine: machinev1.Machine{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "withMatchingLabels",
+					Namespace: "test",
+					Labels: map[string]string{
+						"foo": "bar",
+					},
+				},
+			},
+			expected: false,
+		},
+		{
+			machineSet: machinev1.MachineSet{},
+			machine: machinev1.Machine{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:              "withDeletionTimestamp",
+					Namespace:         "test",
+					DeletionTimestamp: &metav1.Time{Time: time.Now()},
+					Labels: map[string]string{
+						"foo": "bar",
+					},
+				},
+			},
+			expected: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		got := shouldExcludeMachine(&tc.machineSet, &tc.machine)
+		if got != tc.expected {
+			t.Errorf("Case %s. Got: %v, expected: %v", tc.machine.Name, got, tc.expected)
+		}
+	}
+}
+
+func TestHasMatchingLabels(t *testing.T) {
+	testCases := []struct {
+		machineSet machinev1.MachineSet
+		machine    machinev1.Machine
+		expected   bool
+	}{
+		{
+			machineSet: machinev1.MachineSet{
+				Spec: machinev1.MachineSetSpec{
+					Selector: metav1.LabelSelector{
+						MatchLabels: map[string]string{
+							"foo": "bar",
+						},
+					},
+				},
+			},
+			machine: machinev1.Machine{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "matchSelector",
+					Labels: map[string]string{
+						"foo": "bar",
+					},
+				},
+			},
+			expected: true,
+		},
+		{
+			machineSet: machinev1.MachineSet{
+				Spec: machinev1.MachineSetSpec{
+					Selector: metav1.LabelSelector{
+						MatchLabels: map[string]string{
+							"foo": "bar",
+						},
+					},
+				},
+			},
+			machine: machinev1.Machine{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "doesNotMatchSelector",
+					Labels: map[string]string{
+						"no": "match",
+					},
+				},
+			},
+			expected: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		got := hasMatchingLabels(&tc.machineSet, &tc.machine)
+		if tc.expected != got {
+			t.Errorf("Case %s. Got: %v, expected %v", tc.machine.Name, got, tc.expected)
+		}
+	}
+}
+
+func TestUpdateLabelsinMachine(t *testing.T) {
+	testCases := []struct {
+		machineSet     machinev1.MachineSet
+		machine        machinev1.Machine
+		updatedMachine machinev1.Machine
+	}{
+		{
+			machineSet: machinev1.MachineSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test machineset",
+					Namespace: "test",
+				},
+				Spec: machinev1.MachineSetSpec{
+					Template: machinev1.MachineTemplateSpec{
+						Spec: machinev1.MachineSpec{
+							ObjectMeta: machinev1.ObjectMeta{
+								Labels: map[string]string{
+									"foo": "bar"},
+							},
+						},
+					},
+				},
+			},
+			machine: machinev1.Machine{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test machineset",
+					Namespace: "test",
+				},
+				Spec: machinev1.MachineSpec{
+					ObjectMeta: machinev1.ObjectMeta{
+						Labels: map[string]string{},
+					},
+				},
+			},
+			updatedMachine: machinev1.Machine{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test machineset",
+					Namespace: "test",
+				},
+				Spec: machinev1.MachineSpec{
+					ObjectMeta: machinev1.ObjectMeta{
+						Labels: map[string]string{
+							"foo": "bar",
+						},
+					},
+				},
+			},
+		},
+		{
+			machineSet: machinev1.MachineSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test machineset",
+					Namespace: "test",
+				},
+				Spec: machinev1.MachineSetSpec{
+					Template: machinev1.MachineTemplateSpec{
+						Spec: machinev1.MachineSpec{
+							ObjectMeta: machinev1.ObjectMeta{
+								Labels: map[string]string{},
+							},
+						},
+					},
+				},
+			},
+			machine: machinev1.Machine{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test machineset",
+					Namespace: "test",
+				},
+				Spec: machinev1.MachineSpec{
+					ObjectMeta: machinev1.ObjectMeta{
+						Labels: map[string]string{
+							"foo": "bar",
+						},
+					},
+				},
+			},
+			updatedMachine: machinev1.Machine{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test machineset",
+					Namespace: "test",
+				},
+				Spec: machinev1.MachineSpec{
+					ObjectMeta: machinev1.ObjectMeta{
+						Labels: map[string]string{},
+					},
+				},
+			},
+		},
+		{
+			machineSet: machinev1.MachineSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test machineset",
+					Namespace: "test",
+				},
+				Spec: machinev1.MachineSetSpec{
+					Template: machinev1.MachineTemplateSpec{
+						Spec: machinev1.MachineSpec{
+							ObjectMeta: machinev1.ObjectMeta{
+								Labels: map[string]string{},
+							},
+						},
+					},
+				},
+			},
+			machine: machinev1.Machine{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test machineset",
+					Namespace: "test",
+				},
+				Spec: machinev1.MachineSpec{
+					ObjectMeta: machinev1.ObjectMeta{
+						Labels: map[string]string{},
+					},
+				},
+			},
+			updatedMachine: machinev1.Machine{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test machineset",
+					Namespace: "test",
+				},
+				Spec: machinev1.MachineSpec{
+					ObjectMeta: machinev1.ObjectMeta{
+						Labels: map[string]string{},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+
+		err := machinev1.AddToScheme(scheme.Scheme)
+		if err != nil {
+			fmt.Printf("failed adding to scheme")
+		}
+		localObjects := []runtime.Object{
+			&tc.machineSet,
+			&tc.machine,
+		}
+
+		mocks := &mocks{
+			fakeKubeClient: fake.NewFakeClient(localObjects...),
+			mockCtrl:       gomock.NewController(t),
+		}
+		r := ReconcileMachineSet{
+			mocks.fakeKubeClient,
+			scheme.Scheme,
+			record.NewFakeRecorder(32),
+		}
+
+		var ctx context.Context
+		err = r.updateLabelsInMachine(ctx, &tc.machineSet, &tc.machine)
+		if err != nil {
+			t.Error(err)
+		}
+		if !reflect.DeepEqual(tc.machine.Spec.Labels, tc.machineSet.Spec.Template.Spec.Labels) {
+			t.Errorf("Got: %v, expected %v", tc.machine.Spec.Labels, tc.updatedMachine.Spec.Labels)
+		}
+	}
+}
+
+// The test below is failing. WIP
+func TestUpdateLabelsInNode(t *testing.T) {
+	testCases := []struct {
+		machine     machinev1.Machine
+		node        v1.Node
+		updatedNode v1.Node
+	}{
+		{
+			machine: machinev1.Machine{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test machineset",
+					Namespace: "test",
+				},
+				Spec: machinev1.MachineSpec{
+					ObjectMeta: machinev1.ObjectMeta{
+						Labels: map[string]string{
+							"foo": "bar",
+						},
+					},
+				},
+			},
+			node: v1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:        "test-node",
+					Namespace:   "test",
+					Labels:      map[string]string{},
+					Annotations: map[string]string{},
+				},
+			},
+			updatedNode: v1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-node",
+					Namespace: "test",
+					Labels: map[string]string{
+						"foo": "bar",
+					},
+					Annotations: map[string]string{
+						"managed.openshift.com/customlabels": "foo",
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+
+		err := machinev1.AddToScheme(scheme.Scheme)
+		if err != nil {
+			fmt.Printf("failed adding to scheme")
+		}
+		localObjects := []runtime.Object{
+			&tc.machine,
+			&tc.node,
+		}
+
+		mocks := &mocks{
+			fakeKubeClient: fake.NewFakeClient(localObjects...),
+			mockCtrl:       gomock.NewController(t),
+		}
+		r := ReconcileMachineSet{
+			mocks.fakeKubeClient,
+			scheme.Scheme,
+			record.NewFakeRecorder(32),
+		}
+
+		var ctx context.Context
+		err = r.updateLabelsInNode(ctx, &tc.machine)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if !reflect.DeepEqual(tc.machine.Spec.Labels, tc.node.Labels) {
+			t.Errorf("Got: %v, expected %v", tc.node.Labels, tc.updatedNode.Labels)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -25,7 +25,13 @@ import (
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	machinev1 "github.com/openshift/api/machine/v1beta1"
+	"github.com/openshift/managed-node-metadata-operator/controllers"
+	"k8s.io/klog/v2"
+	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -37,37 +43,62 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(machinev1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
 func main() {
 	var metricsAddr string
 	var enableLeaderElection bool
+	var probeAddr string
 	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
+
 	flag.Parse()
 
 	ctrl.SetLogger(zap.New(zap.UseDevMode(true)))
 
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+	opts := ctrl.Options{
 		Scheme:             scheme,
 		MetricsBindAddress: metricsAddr,
 		Port:               9443,
-		LeaderElection:     enableLeaderElection,
-		LeaderElectionID:   "cdb62f5e.my.domain",
-	})
+
+		HealthProbeBindAddress: probeAddr,
+		LeaderElection:         enableLeaderElection,
+		LeaderElectionID:       "cdb62f5e.my.domain",
+		Namespace:              "openshift-machine-api",
+	}
+
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), opts)
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
 		os.Exit(1)
 	}
 
-	// +kubebuilder:scaffold:builder
+	// Setup Scheme for all resources
+	if err := machinev1.AddToScheme(mgr.GetScheme()); err != nil {
+		klog.Fatal(err)
+	}
 
-	setupLog.Info("starting manager")
-	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
-		setupLog.Error(err, "problem running manager")
+	// Setup all Controllers
+	if err := controllers.AddToManager(mgr, opts, controllers.Add); err != nil {
+		klog.Fatal(err)
+	}
+
+	//+kubebuilder:scaffold:builder
+
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up health check")
 		os.Exit(1)
 	}
+	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up ready check")
+		os.Exit(1)
+	}
+
+	// Start the Cmd
+	klog.Fatal(mgr.Start(signals.SetupSignalHandler()))
 }
